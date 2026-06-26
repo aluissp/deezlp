8abd998651586b1ac48baa04857cf74f573aeddf
Luis Perugachi <luisprg05@gmail.com>
2026-06-25 21:06:59 -0500

    feat: added track formats

--- packages/deezer/src/constants/track-formats.ts
+++ packages/deezer/src/constants/track-formats.ts
@@ -1,10 +1,29 @@
-export const TRACK_FORMATS = {
+/** No 360 track formats */
+export const FORMATS_NO_360 = {
+	/** FLAC format, lossless quality */
 	FLAC: 9,
+	/** 320 kbps, high quality */
 	MP3_320: 3,
+	/** 128 kbps, standard quality */
 	MP3_128: 1,
+} as const;
+
+/** Sony 360 Reality Audio formats */
+export const FORMATS_360 = {
+	/** 360 Reality Audio format, ultra high quality */
 	MP4_RA3: 15,
+	/** 360 Reality Audio format, high quality */
 	MP4_RA2: 14,
+	/** 360 Reality Audio format, standard quality */
 	MP4_RA1: 13,
+} as const;
+
+/** Here's all formats, also includes MP3_MISC format (8, 0). */
+export const TRACK_FORMATS = {
+	...FORMATS_NO_360,
+	...FORMATS_360,
+	/** Refers a MP3_MISC format */
 	DEFAULT: 8,
+	/** Refers a MP3_MISC format */
 	LOCAL: 0,
 } as const;
