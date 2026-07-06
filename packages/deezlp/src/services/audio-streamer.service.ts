80c9bf352a63200fd76f0a3facd35c498df9efda
Luis Perugachi <luisprg05@gmail.com>
2026-07-06 12:35:02 -0500

    feat: documented decrypter async generator

--- packages/deezlp/src/services/audio-streamer.service.ts
+++ packages/deezlp/src/services/audio-streamer.service.ts
@@ -118,11 +118,21 @@ export class AudioStreamerService {
 	}
 
 	/**
-	 * Generador Privado: Maneja el descifrado por bloques (Blowfish).
+	 * Decrypts the incoming stream if it is encrypted, otherwise passes it through unchanged.
+	 *
+	 * @param source The source stream of data to be decrypted
+	 * @param isCrypted Indicates whether the stream is encrypted
+	 * @param blowfishKey The Blowfish key used for decryption, if applicable
+	 * @returns
 	 */
-	private async *decrypter(source: AsyncIterable<Buffer>, isCrypted: boolean, blowfishKey?: string) {
+	private async *decrypter(
+		source: AsyncIterable<Buffer>,
+		isCrypted: boolean,
+		blowfishKey?: string,
+	): AsyncGenerator<Buffer<ArrayBufferLike>, void, any> {
+		// if the stream is not encrypted or no blowfish key is provided, yield the source directly
 		if (!isCrypted || !blowfishKey) {
-			yield* source; // Si no está encriptado, pasa el stream directo
+			yield* source;
 			return;
 		}
 
@@ -131,24 +141,78 @@ export class AudioStreamerService {
 			modifiedStream = Buffer.concat([modifiedStream, chunk]);
 
 			while (modifiedStream.length >= 2048 * 3) {
-				// Usamos .subarray() en lugar de .slice() por eficiencia en Buffers modernos
+				/**
+				 * 1. Decrypt the first 6144 bytes of the chunk
+				 * ```text
+				 * 	[ 0, 1, 2, ..., 6143 bytes ]
+				 * 	+-----------------------------------+
+				 * 	| 2048      | 2048      | 2048      |
+				 * 	| Decrypt   | Unchanged | Unchanged |
+				 * 	+-----------------------------------+
+				 * ```
+				 */
 				const decryptingChunks = modifiedStream.subarray(0, 2048 * 3);
+				/**
+				 * 2. Remove the first 6144 bytes from the modified stream
+				 * ```text
+				 * [6144, 6145, 6146, ..., n bytes to keep]
+				 *	+---------------------------------------+
+				 * 	| 0, ..., 6143      | 6144, ... n bytes |
+				 * 	| To remove					| To keep						|
+				 * 	+---------------------------------------+
+				 * ```
+				 */
 				modifiedStream = modifiedStream.subarray(2048 * 3);
-
+				/**
+				 * 3. Decrypt the first 2048 bytes of the chunk
+				 * ```text
+				 * [0, 1, 2, ..., 2047 bytes encrypted]
+				 * ```
+				 */
 				let decryptedChunks = this.cryptoService.decryptChunk(decryptingChunks.subarray(0, 2048), blowfishKey);
+				/**
+				 * 4. Concatenate the decrypted first 2048 bytes with the remaining bytes of the chunk
+				 * ```text
+				 * 	[ 0, ..., 2047 bytes decrypted ] + [ 2048, ..., 6143 bytes unchanged ]
+				 * ```
+				 */
 				decryptedChunks = Buffer.concat([decryptedChunks, decryptingChunks.subarray(2048)]);
 
+				// 5. Yield the decrypted chunk
 				yield decryptedChunks;
 			}
 		}
 
-		// Flush de los bytes restantes
+		// 6. If there are any remaining bytes in the modified stream,
+		// decrypt the first 2048 bytes if possible and yield the result
 		if (modifiedStream.length > 0) {
 			if (modifiedStream.length >= 2048) {
+				/**
+				 * We assuming that the remaining bytes are at least 2048 bytes long
+				 * ```text
+				 * 	[ 0, 1, 2, ..., n bytes where n >= 2048 and n < 6144 ]
+				 * 	+-----------------------------------+
+				 * 	| 2048      | 2049, ..., n bytes    |
+				 * 	| Decrypt   | Unchanged 						|
+				 * 	+-----------------------------------+
+				 * ```
+				 */
 				let decryptedChunks = this.cryptoService.decryptChunk(modifiedStream.subarray(0, 2048), blowfishKey);
+				// Concat with the remaining bytes of the modified stream
 				decryptedChunks = Buffer.concat([decryptedChunks, modifiedStream.subarray(2048)]);
+
 				yield decryptedChunks;
 			} else {
+				/**
+				 * We assuming that the remaining bytes are less than 2048 bytes long, so we yield them unchanged
+				 * ```text
+				 * 	[ 0, 1, 2, ..., n bytes where n < 2048 ]
+				 * 	+-----------------------+
+				 * 	| 0, ..., 2046 bytes    |
+				 * 	| Unchanged 						|
+				 * 	+-----------------------+
+				 * ```
+				 */
 				yield modifiedStream;
 			}
 		}
