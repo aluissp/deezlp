f6174c61d037a0d5f75239f568260a6503962507
Luis Perugachi <luisprg05@gmail.com>
2026-07-14 16:43:16 -0500

    feat: implemented login via arl in cli

--- packages/cli/src/services/login.service.ts
+++ packages/cli/src/services/login.service.ts
@@ -4,14 +4,12 @@ import type { PromptService } from './prompt.service';
 import type { FileService } from './file.service';
 
 export class LoginService {
-
 	constructor(
 		private readonly dl: Deezlp,
 		private readonly logger: LoggerService,
 		private readonly fileService: FileService,
 		private readonly promptService: PromptService,
-	) {
-	}
+	) {}
 
 	async loginViaArl() {
 		if (this.dl.loggedIn) {
@@ -26,20 +24,7 @@ export class LoginService {
 
 		// 2. Try to login with the arl
 		try {
-			const isLogged = await this.dl.loginViaArl(savedArl);
-
-			if (isLogged) {
-				// 3. Save the arl for future logins
-				this.logger.success('Login successful!');
-				this.fileService.saveARL(savedArl);
-				return isLogged;
-			} else {
-				// 4. Remove the invalid arl
-				this.fileService.removeARL();
-				this.logger.error('Login failed, invalid ARL.');
-
-				return isLogged;
-			}
+			return this.loginArl(savedArl);
 		} catch (error) {
 			this.logger.error(`Login failed: ${error}`);
 
@@ -47,6 +32,47 @@ export class LoginService {
 			this.fileService.removeARL();
 			this.logger.error('Login failed, invalid ARL.');
 			return false;
+		}
+	}
+
+	async execute({ arl, logout }: { arl?: string; logout?: boolean }) {
+		if (this.dl.loggedIn && !logout) {
+			// 1. User is already
+			this.logger.warn('You are already logged in.');
+			return;
+		} else if (this.dl.loggedIn && logout) {
+			// 2. Logout user
+			this.fileService.removeARL();
+			this.logger.success('Logging out from your account.');
+			return;
+		}
+
+		// 3. Login user with arl
+		if (arl) {
+			await this.loginArl(arl);
+			return;
+		}
+
+		await this.loginViaArl();
+
+		// 4. Login user with credentials
+	}
+
+	/** Login using arl token */
+	private async loginArl(arl: string) {
+		const isLogged = await this.dl.loginViaArl(arl);
+
+		if (isLogged) {
+			// 3. Save the arl for future logins
+			this.logger.success('Login successful!');
+			this.fileService.saveARL(arl);
+			return isLogged;
+		} else {
+			// 4. Remove the invalid arl
+			this.fileService.removeARL();
+			this.logger.error('Login failed, invalid ARL.');
+
+			return isLogged;
 		}
 	}
 }
